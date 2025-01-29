d4ab24acb3a8228917e24de77b2b65a215f1c73f
avereshchaga <avereshchaga@gmail.com>
2025-01-29 13:53:04 +0300

    * 1. Исправление ошибки ** Замена нерабочего api

--- flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
@@ -1,18 +1,28 @@
 package otus.homework.flowcats
 
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class CatsRepository(
     private val catsService: CatsService,
-    private val refreshIntervalMs: Long = 5000
+    private val refreshIntervalMs: Long = 2000
 ) {
 
     fun listenForCatFacts() = flow {
         while (true) {
-            val latestNews = catsService.getCatFact()
-            emit(latestNews)
-            delay(refreshIntervalMs)
+            runCatching { catsService.getCatFact() }
+                .onSuccess {
+                    emit(it)
+                    delay(refreshIntervalMs)
+                }
+                .onFailure {
+                    println(it.toString())
+                    Log.e("CatsTag", "Error", it)
+                    delay(refreshIntervalMs)
+                }
         }
-    }
+    }.flowOn(Dispatchers.IO)
 }
